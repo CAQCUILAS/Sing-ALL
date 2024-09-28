ae0e8f63e18f954f490f83e2f6f81a5a20d5fb10
AquilasAKP <aquilasakpaki0@gmail.com>
2024-09-28 23:49:15 +0100

    J'ai créé une vue dans view et appelé dans url et cela marche bien

--- singall/singall/urls.py
+++ singall/singall/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from acceuil import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', views.hello),
+    path('about/', views.about),
+    path('listing/', views.listings),
+    path('contact/', views.contact)
 ]
